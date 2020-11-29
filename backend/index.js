5fa437e8d66ab052ac68e7562d7121d8345c16c4
ElMayo <mic.l.meier@gmail.com>
2020-11-29 16:07:07 +0100

    chore: Set up development environment

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
-console.log('test')
+import express from 'express'
+const app = express()
+
+app.get('/', (req, res) => {
+  res.json({ content: 'Hello World' })
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
