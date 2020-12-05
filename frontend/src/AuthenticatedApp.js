73381fdfb9eac91599fb4cfcee4dbddd05dd65a0
ElMayo <mic.l.meier@gmail.com>
2020-12-05 12:15:53 +0100

    feat: Add theme-ui and theme

--- frontend/src/AuthenticatedApp.js
+++ frontend/src/AuthenticatedApp.js
@@ -1,11 +1,17 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
 import { useAuth0 } from '@auth0/auth0-react'
 import React from 'react'
+import { jsx } from 'theme-ui'
 
 function LogoutButton() {
   const { logout } = useAuth0()
 
   return (
-    <button onClick={() => logout({ returnTo: window.location.origin })}>
+    <button
+      sx={{ variant: 'buttons.simple' }}
+      onClick={() => logout({ returnTo: window.location.origin })}
+    >
       Log Out
     </button>
   )
