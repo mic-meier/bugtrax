bc22f7ae647acbc2651686bd122fc2f1bf305a7f
ElMayo <mic.l.meier@gmail.com>
2020-12-05 15:39:21 +0100

    feat: Add full page loadging spinner

--- frontend/src/components/loading.js
+++ frontend/src/components/loading.js
@@ -1,10 +1,39 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
+import { keyframes } from '@emotion/core'
+import styled from '@emotion/styled/macro'
 import React from 'react'
+import { ImSpinner2 } from 'react-icons/im'
 import { jsx } from 'theme-ui'
 
+const spin = keyframes({
+  '0%': { transform: 'rotate(0deg)' },
+  '100%': { transform: 'rotate(360deg)' },
+})
+
+const Spinner = styled(ImSpinner2)({
+  animation: `${spin} 1s linear infinite`,
+})
+Spinner.defaultProps = {
+  'aria-label': 'loading',
+}
+
 function Loading() {
-  return <div>Loading...</div>
+  return (
+    <div
+      sx={{
+        fontSize: 10,
+        height: 'screenHeight',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: 'danger',
+      }}
+    >
+      <Spinner />
+    </div>
+  )
 }
 
 export default Loading
